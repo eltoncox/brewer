17662f01976b01fafbba2adebfb17d454d312a56
Elton Luiz <elton.unix@gmail.com>
2023-10-09 11:17:42 -0300

    projeto construído até a configuração do Log4J2.

---
+++ src/main/java/com/elton/cervejas/controller/CervejasController.java
@@ -0,0 +1,43 @@
+package com.elton.cervejas.controller;
+
+
+
+import javax.validation.Valid;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.elton.cervejas.model.Cerveja;
+
+@Controller
+public class CervejasController {
+	
+	private static Logger logger = LoggerFactory.getLogger(CervejasController.class);
+
+	@RequestMapping("/cervejas/novo")
+	public String novo(Cerveja cerveja) {
+		logger.error("Aqui é um log nível error!");
+		logger.info("Aqui é um logger nível info");		
+		
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return novo(cerveja);
+		}
+		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		System.out.println(">>> sku: " + cerveja.getSku());
+		return "redirect:/cervejas/novo";
+	}
+}
